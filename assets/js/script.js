a5c5a85baba7791c050884f5be7b882b84129d46
BaoPhan-st <23130023@st.hcmuaf.edu.vn>
2025-11-01 16:54:33 +0700

    speacial deal

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,20 +18,3 @@ for (let i = 0; i < navElems.length; i++) {
   });
 }
 
-
-/**
- * header & go top btn active on page scroll
- */
-
-const header = document.querySelector("[data-header]");
-const goTopBtn = document.querySelector("[data-go-top]");
-
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 80) {
-    header.classList.add("active");
-    goTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    goTopBtn.classList.remove("active");
-  }
-});
